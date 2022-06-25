32441f8cd13054ed290d0f1b7543877820ab7302
earlpappi <earlpappi@gmail.com>
2022-06-25 21:27:07 +0100

    Login and Signup fixed?

--- src/Components/Auth/Signup.jsx
+++ src/Components/Auth/Signup.jsx
@@ -1,8 +1,19 @@
-
+import classes from './Auth.module.css'
 
 const Signup = () => {
     return ( 
-        <h1>This is sign up page </h1>
+        <form action="" className={ classes.signupForm }>
+            <div className={ classes.signupFlex }>
+                <input type="text" placeholder='First Name'/>
+                <input type="text" placeholder='Last Name'/>
+            </div>
+
+            <input type="email" placeholder='Email'/>
+            <input type="tel" placeholder='Phone Number'/>
+            <a href="./">I have a patner code</a>
+
+            <button>Register</button>
+        </form>
      );
 }
  
