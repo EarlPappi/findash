318f2eb90a5643aac683c82173abce663f9501bf
earlpappi <earlpappi@gmail.com>
2022-06-27 20:23:45 +0100

    buliding -- dir

--- src/Components/Layout/Directory/Account.jsx
+++ src/Components/Layout/Directory/Account.jsx
@@ -1,8 +1,29 @@
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faToggleOff } from '@fortawesome/free-solid-svg-icons'
 const Account = () => {
     return ( 
         <div>
             <h1>Account</h1>
+
+            <div>
+                <div>
+                    <span>$0.00</span>
+                    <span><FontAwesomeIcon icon={faToggleOff}/> Total in Real Account</span>
+                    <span>$0.00</span>
+                </div>
+
+                <div>
+                    <span>$0.00</span>
+                    <span><FontAwesomeIcon icon={faToggleOff}/> Total in Real Account</span>
+                    <span>$0.00</span>
+                </div>
+                
+                <div>
+                    <span>$0.00</span>
+                    <span><FontAwesomeIcon icon={faToggleOff}/> Total in Real Account</span>
+                    <span>$0.00</span>
+                </div>
+            </div>
         </div>
      );
 }
